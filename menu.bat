d31eb3ed8444e56509b1a988859590413b244e00
Maurits <github.com.4ex3i1ts.online@maurits.tv>
2016-01-01 17:40:45 +0100

    Moved executable to separate folder and added Lua 5.1 executable

--- menu.bat
+++ menu.bat
@@ -1,3 +1,26 @@
 @echo off
 
-lua52.exe "./menu.lua"
+goto menu
+
+:lua51
+start bin/lua51.exe "./menu.lua"
+exit
+
+:lua52
+start bin/lua52.exe "./menu.lua"
+exit
+
+:menu
+
+echo ------------------------------
+echo -- Launch with lua version:
+echo ------------------------------
+echo 1. Lua 5.1
+echo 2. Lua 5.2
+echo 3. Exit
+echo ------------------------------
+SET /P M=Make a choice:
+IF %M%==1 GOTO lua51
+IF %M%==2 GOTO lua52
+IF %M%==3 exit
+
