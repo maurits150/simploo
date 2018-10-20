eaef53aa7258a899bba8014cdf5d555c19ff410f
Maurits <public@maurits.tv>
2018-10-20 17:11:53 +0200

    issue #7 fix bug in ambiguous state of members

--- tests/instancer_multi_inheritance.lua
+++ tests/instancer_multi_inheritance.lua
@@ -1,7 +1,36 @@
 TestInstancerMultipleInheritance = {}
 
-function TestInstancerMultipleInheritance:testInstantiation()
-    
+function TestInstancerMultipleInheritance:testOverrides()
+	A_CALLED = false
+	B_CALLED = false
+	C_CALLED = false
+
+	class "A" {
+	    func = function(self)
+	        A_CALLED = true
+	    end;
+	}
+
+	class "B" {
+	    func = function(self)
+	        B_CALLED = true
+	    end;
+	}
+
+	class "C" extends "A, B" {
+	    func = function(self)
+	        self.A:func()
+	        self.B:func()
+
+	        C_CALLED = true
+	    end;
+	}
+
+	C.new():func()
+
+	assertTrue(A_CALLED)
+	assertTrue(B_CALLED)
+	assertTrue(C_CALLED)
 end
 
 LuaUnit:run("TestInstancerMultipleInheritance")
