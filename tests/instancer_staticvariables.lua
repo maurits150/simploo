5e060390a7fe5deae667c63e904944da7c94cfb5
Maurits <public@maurits.tv>
2024-03-17 19:12:37 +0100

    Broader test for static variables

--- tests/instancer_staticvariables.lua
+++ tests/instancer_staticvariables.lua
@@ -1,18 +1,40 @@
 function Test:testStaticVariables()
     class "A" {
         static {
-            staticvar = 1;
-        }
+            staticvar = "unset";
+        };
+
+        set = function(self)
+            self.staticvar = "set"
+        end;
+
+        getValue = function(self)
+            return self.staticvar;
+        end
     }
 
     -- check current val on instance 1
     local instance1 = A.new()
-    assertEquals(instance1.staticvar, 1)
+    assertEquals(instance1.staticvar, "unset")
+    assertEquals(instance1:getValue(), "unset")
 
     -- change val on instance 1
-    instance1.staticvar = 2
+    instance1:set()
+
+    -- check value on instance 1
+    assertEquals(instance1.staticvar, "set")
+    assertEquals(instance1:getValue(), "set")
 
     -- check if val propagated to instance 2
     local instance2 = A.new()
-    assertEquals(instance2.staticvar, 2)
+    assertEquals(instance2.staticvar, "set")
+    assertEquals(instance2:getValue(), "set")
+
+    -- change val on instance 2 via public accessor
+    instance2.stativar = "set directly"
+
+    -- check if val propagated to instance 3
+    local instance3 = A.new()
+    assertEquals(instance3.staticvar, "set")
+    assertEquals(instance3:getValue(), "set")
 end
